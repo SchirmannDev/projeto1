a69265168593bb874bae10582535190ce4a9f72d
schirmann <cschirmann.dev@gmail.com>
2021-04-05 09:50:23 -0300

    estilizando a imagem

--- App.js
+++ App.js
@@ -11,14 +11,14 @@ export default class PrimeiroProjeto extends Component {
 
     return (
       <View style={{ marginTop: 50 }}>
-        <Text style={{ fontSize: 25, color: "red", margin: 20 }}>
+        <Text style={{ fontSize: 25, color: "red", margin: 50 }}>
           Hello World
         </Text>
         <Image
           source={{
             uri: "https://github.com/schirmann.png",
           }}
-          style={{ width: 300, height: 300 }}
+          style={{ width: 300, height: 300, borderRadius: 150, margin: 50 }}
         />
       </View>
     );
