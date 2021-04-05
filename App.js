86c568903b3f76641791c2deed316ca05768c074
schirmann <cschirmann.dev@gmail.com>
2021-04-05 20:22:44 -0300

    inserindo dados e recebendo com props

--- App.js
+++ App.js
@@ -2,10 +2,16 @@ import React, { Component } from "react";
 import { View, Text, TextInput, StyleSheet } from "react-native";
 
 export default class Projeto1 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { texto: "" };
+  }
+
   render() {
     return (
-      <View>
-        <TextInput style={styles.input} />
+      <View style={{ marginTop: 30 }}>
+        <TextInput style={styles.input} placeholder="Qual seu nome?" />
+        <Text style={styles.texto}>{this.state.texto}</Text>
       </View>
     );
   }
@@ -16,7 +22,11 @@ const styles = StyleSheet.create({
     height: 40,
     borderWidth: 1,
     borderColor: "#00ff",
-    margin: 50,
+    margin: 10,
     padding: 10,
+  },
+  texto: {
+    fontSize: 20,
+    textAlign: "center",
   },
 });
