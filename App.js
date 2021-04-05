5100ecd41f1edc3abf48a50478d034fc6c796831
schirmann <cschirmann.dev@gmail.com>
2021-04-05 15:48:43 -0300

    tamanho fixo e dinamico

--- App.js
+++ App.js
@@ -5,13 +5,11 @@ export default class Projeto1 extends Component {
   render() {
     return (
       <View style={{ marginTop: 20, flex: 1, backgroundColor: "yellow" }}>
-        <View style={{ width: 50, height: 50, backgroundColor: "blue" }}></View>
-        <View
-          style={{ width: 100, height: 100, backgroundColor: "green" }}
-        ></View>
-        <View
-          style={{ width: 200, height: 200, backgroundColor: "red" }}
-        ></View>
+        <View style={{ flex: 1, backgroundColor: "blue" }}></View>
+        <View style={{ flex: 1, backgroundColor: "green" }}></View>
+        <View style={{ flex: 1, backgroundColor: "red" }}></View>
+        <View style={{ flex: 2, backgroundColor: "gray" }}></View>
+        <View style={{ flex: 1, backgroundColor: "pink" }}></View>
       </View>
     );
   }
