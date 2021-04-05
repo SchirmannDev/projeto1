3e5f61cca3b03535e422f01930068544e38000b4
schirmann <cschirmann.dev@gmail.com>
2021-04-05 20:13:57 -0300

    recebendo dados do usuario

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, TextInput, StyleSheet } from "react-native";
 
 export default class Projeto1 extends Component {
   render() {
     return (
-      <View style={{ marginTop: 20, flex: 1, backgroundColor: "yellow" }}>
-        <View style={{ flex: 1, backgroundColor: "blue" }}></View>
-        <View style={{ flex: 1, backgroundColor: "green" }}></View>
-        <View style={{ flex: 1, backgroundColor: "red" }}></View>
-        <View style={{ flex: 2, backgroundColor: "gray" }}></View>
-        <View style={{ flex: 1, backgroundColor: "pink" }}></View>
+      <View>
+        <TextInput style={styles.input} />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    borderWidth: 1,
+    borderColor: "#00ff",
+    margin: 50,
+    padding: 10,
+  },
+});
