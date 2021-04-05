2ff0ec2814e953e8b19354f73164dda792d20803
schirmann <cschirmann.dev@gmail.com>
2021-04-05 10:07:46 -0300

    criando o proprio componente

--- App.js
+++ App.js
@@ -1,24 +1,31 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
 
-export default class PrimeiroProjeto extends Component {
-  somar(n1, n2) {
-    return n1 + n2;
-  }
-
+class Imagem extends Component {
   render() {
-    let nome = "Cintia";
+    let imagem = {
+      uri: "https://github.com/'+this.props.nome+'.png",
+    };
+
+    return <Image source={imagem} style={{ width: 300, height: 300 }} />;
+  }
+}
+
+export default class Projeto1 extends Component {
+  render() {
+    let imagem = {
+      uri: "https://github.com/schirmann.png",
+    };
 
     return (
       <View style={{ marginTop: 50 }}>
         <Text style={{ fontSize: 25, color: "red", margin: 50 }}>
           Hello World
         </Text>
+        <Imagem nome="schirmann" />
         <Image
-          source={{
-            uri: "https://github.com/schirmann.png",
-          }}
-          style={{ width: 300, height: 300, borderRadius: 150, margin: 50 }}
+          source={imagem}
+          style={{ width: 300, height: 300, borderRadius: 150 }}
         />
       </View>
     );
