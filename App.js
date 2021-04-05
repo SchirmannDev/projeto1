d4d60b1d73d36015d8f572ee33fb46608f19c3d5
schirmann <cschirmann.dev@gmail.com>
2021-04-05 13:32:42 -0300

    aplicando const para cores

--- App.js
+++ App.js
@@ -6,9 +6,9 @@ export default class Projeto1 extends Component {
     return (
       <View style={{ paddingTop: 50 }}>
         <Text style={styles.azulGrande}>Hello World</Text>
-        <Text>Olá Mundo</Text>
-        <Text>React</Text>
-        <Text>React-native</Text>
+        <Text style={styles.vermelho}>Olá Mundo</Text>
+        <Text style={styles.amarelo}>React</Text>
+        <Text style={styles.pink}>React-native</Text>
       </View>
     );
   }
@@ -18,6 +18,24 @@ const styles = StyleSheet.create({
   azulGrande: {
     color: "#0000ff",
     fontSize: 30,
+    textAlign: "center",
+  },
+  vermelho: {
+    color: "#ff0000",
+    fontSize: 25,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  amarelo: {
+    color: "#ff0",
+    fontSize: 20,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  pink: {
+    color: "#ff00ff",
+    fontSize: 25,
+    fontWeight: "bold",
     textAlign: "center",
   },
 });
