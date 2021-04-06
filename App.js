b13473065e4a11f945ff09aab7b9952c1a52cdfa
schirmann <cschirmann.dev@gmail.com>
2021-04-06 16:00:16 -0300

    criando estilos

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 
 export default class Projeto1 extends Component {
   render() {
-    return <View style={StyleSheet.body}></View>;
+    return (
+      <View style={styles.body}>
+        <View>
+          <Text style={styles.titulo}>Criador de Memes</Text>
+        </View>
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
-  body: {},
+  body: {
+    backgroundColor: "#999999",
+    paddingTop: 30,
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  titulo: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "blue",
+  },
 });
