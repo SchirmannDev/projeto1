4f101b2758d85788f51fc0f231ff31de1baa64f0
schirmann <cschirmann.dev@gmail.com>
2021-04-06 15:46:43 -0300

    criando app de memes

--- App.js
+++ App.js
@@ -1,54 +1,12 @@
 import React, { Component } from "react";
-import { View, Text, Button, Alert, TextInput, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 
 export default class Projeto1 extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { inputTexto: "", texto: "" };
-
-    this.apertouBotão = this.apertouBotão.bind(this);
-  }
-
-  apertouBotão() {
-    let s = this.state;
-
-    if (s.inputTexto == "Cintia") {
-      s.texto = "Vocẽ acertou";
-    } else {
-      s.texto = "Você errou";
-    }
-
-    this.setState(s);
-  }
-
   render() {
-    return (
-      <View style={{ marginTop: 30 }}>
-        <TextInput
-          style={styles.input}
-          placeholder="Adivinhe meu nome..."
-          onChangeText={(inputTexto) => this.setState({ inputTexto })}
-        />
-
-        <Button title="Aperte em mim" onPress={this.apertouBotão} />
-
-        <Text style={styles.texto}>{this.state.texto}</Text>
-      </View>
-    );
+    return <View style={StyleSheet.body}></View>;
   }
 }
 
 const styles = StyleSheet.create({
-  input: {
-    height: 40,
-    borderWidth: 1,
-    borderColor: "#00ff",
-    margin: 10,
-    padding: 10,
-  },
-  texto: {
-    fontSize: 25,
-    textAlign: "center",
-    color: "#ff0000",
-  },
+  body: {},
 });
