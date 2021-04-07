cb8ed12c44ad95c4d4efa46937d9c4cbea2204c0
schirmann <cschirmann.dev@gmail.com>
2021-04-07 15:13:53 -0300

    projeto finalizado

--- App.js
+++ App.js
@@ -13,6 +13,11 @@ export default class Projeto1 extends Component {
     let novoTexto = texto.toLowerCase();
 
     novoTexto = novoTexto.replace(/(a|e|i|o|u)/g, "i");
+    novoTexto = novoTexto.replace(/(á|à|ã|â)/g, "i");
+    novoTexto = novoTexto.replace(/(é|è|ê)/g, "i");
+    novoTexto = novoTexto.replace(/(í|ì|î)/g, "i");
+    novoTexto = novoTexto.replace(/(ó|ò|ô)/g, "i");
+    novoTexto = novoTexto.replace(/(ú|ù|û)/g, "i");
 
     return novoTexto;
   }
